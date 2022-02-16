29b5cd11b038174393882b58d71e2328942a7552
pervaiz <m.akhtar@hotmail.co.uk>
2022-02-16 21:45:07 +0000

    initial

---
+++ src/Speakers.js
@@ -0,0 +1,24 @@
+import { useQuery, gql } from "@apollo/client";
+const getSpeakers = gql`	
+ query  getSpeakers {
+    speakers {
+      bio,
+      name,
+      id
+    }
+ } 
+`;
+function Speakers(){
+    const { loading, error, data} = useQuery(getSpeakers);
+    if(loading) return <p>Loading...</p>
+    if(error) return <p>Error...</p>
+    console.log(data);
+    return data.speakers.map(({id,name}) => {
+        <div key={id}>
+            <h4>List of speakers</h4>
+            <p>Id:{id}</p>
+            <p>Name : {name}</p>
+        </div>
+    });
+}
+export default Speakers;
