6710a6cf62f89d08803a1b81f820309865c40e76
pervaiz <m.akhtar@hotmail.co.uk>
2022-03-29 13:41:24 +0000

    tidyp

--- src/Components/TopMenu/Menu.jsx
+++ src/Components/TopMenu/Menu.jsx
@@ -13,63 +13,38 @@ export const Menu = () =>{
 }
 
 const items = [
-    {
-       label:'File',
-       icon:'pi pi-fw pi-file',
-       items:[
-          {
-             label:'New',
-             icon:'pi pi-fw pi-plus',
-             items:[
-                {
-                   label:'Bookmark',
-                   icon:'pi pi-fw pi-bookmark'
-                },
-                {
-                   label:'Video',
-                   icon:'pi pi-fw pi-video'
-                },
+   {
+      label:'Attendees',
+      icon:'pi pi-fw pi-user',
+      items:[
+         {
+            label:'New',
+            icon:'pi pi-fw pi-user-plus',
 
-             ]
-          },
-          {
-             label:'Delete',
-             icon:'pi pi-fw pi-trash'
-          },
-          {
-             separator:true
-          },
-          {
-             label:'Export',
-             icon:'pi pi-fw pi-external-link'
-          }
-       ]
-    },
-    {
-       label:'Edit',
-       icon:'pi pi-fw pi-pencil',
-       items:[
-          {
-             label:'Left',
-             icon:'pi pi-fw pi-align-left'
-          },
-          {
-             label:'Right',
-             icon:'pi pi-fw pi-align-right'
-          },
-          {
-             label:'Center',
-             icon:'pi pi-fw pi-align-center'
-          },
-          {
-             label:'Justify',
-             icon:'pi pi-fw pi-align-justify'
-          },
+         },
+         {
+            label:'Delete',
+            icon:'pi pi-fw pi-user-minus',
 
-       ]
-    },
+         },
+         {
+            label:'Search',
+            icon:'pi pi-fw pi-users',
+            items:[
+               {
+                  label:'Filter',
+                  icon:'pi pi-fw pi-filter'
+               },
+               {
+                  icon:'pi pi-fw pi-bars',
+                  label:'List'
+               }
+            ]
+         }
+      ]
+   },
     {
-       label:'Users',
+       label:'Speakers',
        icon:'pi pi-fw pi-user',
        items:[
           {
@@ -88,13 +63,7 @@ const items = [
              items:[
                 {
                    label:'Filter',
-                   icon:'pi pi-fw pi-filter',
-                   items:[
-                      {
-                         label:'Print',
-                         icon:'pi pi-fw pi-print'
-                      }
-                   ]
+                   icon:'pi pi-fw pi-filter'
                 },
                 {
                    icon:'pi pi-fw pi-bars',
@@ -105,7 +74,7 @@ const items = [
        ]
     },
     {
-       label:'Events',
+       label:'Sessions',
        icon:'pi pi-fw pi-calendar',
        items:[
           {
@@ -122,21 +91,7 @@ const items = [
                 },
 
              ]
-          },
-          {
-             label:'Archieve',
-             icon:'pi pi-fw pi-calendar-times',
-             items:[
-                {
-                   label:'Remove',
-                   icon:'pi pi-fw pi-calendar-minus'
-                }
-             ]
           }
        ]
-    },
-    {
-       label:'Quit',
-       icon:'pi pi-fw pi-power-off'
     }
  ];
