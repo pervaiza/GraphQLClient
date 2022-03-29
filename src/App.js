c0dbe4b35f39792baa868eb585015e7bf1152fb5
pervaiz <m.akhtar@hotmail.co.uk>
2022-03-29 15:44:26 +0000

    added formIK

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import { Speakers } from './Components/speakers';
 import { Menu } from './Components/topMenu';
+import {AddSpeaker} from './Components/speakers/speakerForm';
+
 function App() {
   return (
     <div className="App">
       <Menu />
+      <AddSpeaker />
       <Speakers />
     </div>
   );
---
+++ src/Components/speakers/speakerForm/index.js
@@ -0,0 +1,1 @@
+export {AddSpeaker} from './addSpeaker';
