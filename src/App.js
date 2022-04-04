8e65f3ce9986247d5e2a11fd9037fdc4f203ae10
pervaiz <m.akhtar@hotmail.co.uk>
2022-04-04 04:16:39 +0000

    revert changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 import { Speakers } from './Components/speakers';
 import { Menu } from './Components/topMenu';
-import {AddSpeaker} from './Components/speakers/speakerForm';
 
 function App() {
   return (
     <div className="App">
       <Menu />
-      <AddSpeaker />
       <Speakers />
     </div>
   );
